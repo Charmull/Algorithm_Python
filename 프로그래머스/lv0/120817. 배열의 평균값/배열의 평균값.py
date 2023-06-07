8d641de6be6700c05d7b018f6c27fcb19e521258
Charmull <527wlals@gmail.com>
2023-06-07 21:57:06 +0900

    [level 0] Title: 배열의 평균값, 풀이 방법 추가

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,6 +1,15 @@
+# list 요소의 합을 구하는 함수 sum 사용
+def solution(numbers):
+    answer = sum(numbers) / len(numbers)
+    return answer
+
+
+"""
+# for문 사용
 def solution(numbers):
     answer = 0
     for i in numbers:
         answer += i
     answer /= len(numbers)
     return answer
+"""
