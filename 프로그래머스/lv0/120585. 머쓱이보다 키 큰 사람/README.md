6e1704d60877d2b2aa2caa5397523cecfd421c4d
Charmull <527wlals@gmail.com>
2023-06-09 05:17:26 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, 풀이 방법 추가

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/README.md
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/README.md
@@ -1,6 +1,8 @@
-# [level 0] 머쓱이보다 키 큰 사람 - 120585 
+# [level 0] 머쓱이보다 키 큰 사람 - 120585
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120585)
+
+✅ [블로그 포스팅 (Charmull)]()
 
 ### 성능 요약
 
@@ -65,6 +67,5 @@ Empty
 <ul>
 <li>180, 120, 140 중 190보다 큰 수는 없으므로 0명입니다.</li>
 </ul>
-
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -2,5 +2,21 @@ def solution(array, height):
     array.sort(reverse=True)
     answer = 0
     for val in array:
-        if val > height: answer += 1
+        if val > height:
+            answer += 1
     return answer
+
+
+"""
+# 배열의 메소드들 활용하기
+def solution(array, height):
+	array.append(height)
+    array.sort(reverse=True)
+    return array.index(height)
+"""
+
+"""
+# 배열의 메소드들 활용하기
+def solution(array, height):
+	return sum(1 for a in array if a > height)
+"""
