92cdfe938dd39b2347206103c4cda950acfde1b2
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-11 23:34:49 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    engToNum = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    for i in engToNum:
+        numbers = numbers.replace(i, str(engToNum[i]))
+    return int(numbers)
