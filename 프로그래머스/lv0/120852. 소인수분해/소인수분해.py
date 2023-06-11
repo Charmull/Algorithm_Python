37a98341ea8dcfa704ffa63bb16cc62aec251b13
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-11 22:45:20 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = []
+    for i in range(2, n + 1):
+        if (n <= 1): break
+        
+        isprime = True
+        for j in range(2, i):
+            if i % j == 0:
+                isprime = False
+                break
+        if isprime:
+            if n % i == 0: answer.append(i)
+            while(n % i == 0):
+                n /= i
+    return answer
