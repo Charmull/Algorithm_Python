2f2b8d80d10dd3b580ac50606a0b620eba2b38ba
Charmull <527wlals@gmail.com>
2023-06-14 01:00:17 +0900

    [level 0] Title: 팩토리얼, 풀이 방법 정리

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -1,13 +1,6 @@
 def solution(n):
-    if n == 0: return 0
-    answer = 0
-    total = 1
-    for i in range(1, 11):
-        total *= i
-        if total == n:
-            answer = i
-            break
-        elif total > n:
-            answer = i - 1
-            break
-    return answer
+    answer = factorial = 1
+    while n >= factorial:
+        answer += 1
+        factorial *= answer
+    return answer - 1
