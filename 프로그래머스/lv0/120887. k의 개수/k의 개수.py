82dc958bd011e0ca8e0eb46ea25118d93f6b7bae
Charmull <527wlals@gmail.com>
2023-06-14 01:13:42 +0900

    [level 0] Title: k의 개수, 풀이 방법 정리

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,7 +1,5 @@
 def solution(i, j, k):
     answer = 0
     for v in range(i, j + 1):
-        for v2 in list(str(v)):
-            if v2 == str(k):
-                answer += 1
+        answer += str(v).count(str(k))
     return answer
