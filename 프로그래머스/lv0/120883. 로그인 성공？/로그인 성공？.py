ee44c6d172d038c4f631bc062299f0a220901873
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-15 19:04:48 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,8 +1,8 @@
 def solution(id_pw, db):
     for data in db:
-        if id_pw[0] == data[0]:
-            if id_pw[1] == data[1]:
-                return "login"
-            else:
-                return "wrong pw"
+        if id_pw[0] != data[0]:
+            continue
+        if id_pw[1] != data[1]:
+            return "wrong pw"
+        return "login"
     return "fail"
