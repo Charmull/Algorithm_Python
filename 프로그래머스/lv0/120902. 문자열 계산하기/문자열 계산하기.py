d958b53ad86712d21863f31fa3e8da1b56cdb301
Charmull <527wlals@gmail.com>
2023-06-15 18:30:27 +0900

    [level 0] Title: 문자열 계산하기, 풀이 방법 정리

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,6 +1,18 @@
 def solution(my_string):
     my_list = my_string.split()
     answer = int(my_list[0])
+    for i in range(1, len(my_list), 2):
+        if my_list[i] == '+':
+            answer += int(my_list[i + 1])
+        else:
+            answer -= int(my_list[i + 1])
+    return answer
+
+
+"""
+def solution(my_string):
+    my_list = my_string.split()
+    answer = int(my_list[0])
     for i in my_list[1:]:
         try:
             operand = int(i)
@@ -10,18 +22,6 @@ def solution(my_string):
                 answer -= operand
         except:
             operator = i
-    return answer
-
-
-"""
-def solution(my_string):
-    my_list = my_string.split()
-    answer = int(my_list[0])
-    for i in range(1, len(my_list), 2):
-        if my_list[i] == '+':
-            answer += int(my_list[i + 1])
-        else:
-            answer -= int(my_list[i + 1])
     return answer
 """
 
