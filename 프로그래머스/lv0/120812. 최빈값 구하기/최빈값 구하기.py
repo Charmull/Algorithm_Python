a21fc964b31a1438d620ab873f1f9a8bb25bb2b3
Charmull <527wlals@gmail.com>
2023-06-16 19:45:04 +0900

    [level 0] Title: 최빈값 구하기, 풀이 방법 추가

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -9,3 +9,15 @@ def solution(array):
         elif count != 0 and array.count(i) == count:
             answer = -1
     return answer
+
+
+"""
+import collections
+
+def solution(array):
+    d = collections.Counter(array).most_common(2)
+    print(d)
+    if len(d) == 1 or d[0][1] != d[1][1]:
+        return d[0][0]
+    return -1
+"""
