2d015318aa2471d274e19d8ff54bf16409d570d0
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-16 19:36:37 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,23 +1,15 @@
 def solution(polynomial):
-    inputList = polynomial.split()
+    inputList = polynomial.split(' + ')
     valX = 0
     valInt = 0
     for v in inputList:
-        if v[-1] == 'x' and len(v) > 1:
-            valX += int(v[:len(v) - 1])
-        elif v[-1] == 'x':
-            valX += 1
-        elif v == '+':
-            pass
+        if v[-1] == 'x':
+            valX += 1 if v == 'x' else int(v[:-1])
         else:
             valInt += int(v)
-    answer = ''
-    if valX == 1:
-        answer += 'x'
-    elif valX != 0:
-        answer += str(valX) + 'x'
-    if valX != 0 and valInt != 0:
-        answer += ' + ' + str(valInt)
-    elif valX == 0 and valInt != 0:
-        answer = str(valInt)
-    return answer
+            
+    if valX == 0:
+        return str(valInt)
+    elif valX == 1:
+        return 'x' if valInt == 0 else f'x + {valInt}'
+    return f'{valX}x' if valInt == 0 else f'{valX}x + {valInt}'
