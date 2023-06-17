f8948a74f186d86925a5058df03d3ded375d2be0
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-17 22:45:51 +0900

    [level 0] Title: 안전지대, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,46 +1,18 @@
-def check_danger(board, i, j):
-    if i - 1 < 0 and j - 1 < 0:
-        board[i][j] = board[i][j + 1] = 2
-        board[i + 1][j] = board[i + 1][j + 1] = 2
-    elif i - 1 < 0 and j + 1 == len(board):
-        board[i][j - 1] = board[i][j] = 2
-        board[i + 1][j - 1] = board[i + 1][j] = 2
-    elif i + 1 == len(board) and j - 1 < 0:
-        board[i - 1][j] = board[i - 1][j + 1] = 2
-        board[i][j] = board[i][j + 1] = 2
-    elif i + 1 == len(board) and j + 1 == len(board):
-        board[i - 1][j - 1] = board[i - 1][j] = 2
-        board[i][j - 1] = board[i][j] = 2
-    elif i - 1 < 0:
-        board[i][j - 1] = board[i][j] = board[i][j + 1] = 2
-        board[i + 1][j - 1] = board[i + 1][j] = board[i + 1][j + 1] = 2
-    elif i + 1 == len(board):
-        board[i - 1][j - 1] = board[i - 1][j] = board[i - 1][j + 1] = 2
-        board[i][j - 1] = board[i][j] = board[i][j + 1] = 2
-    elif j - 1 < 0:
-        board[i - 1][j] = board[i - 1][j + 1] = 2
-        board[i][j] = board[i][j + 1] = 2
-        board[i + 1][j] = board[i + 1][j + 1] = 2
-    elif j + 1 == len(board):
-        board[i - 1][j - 1] = board[i - 1][j] = 2
-        board[i][j - 1] = board[i][j] = 2
-        board[i + 1][j - 1] = board[i + 1][j] = 2
-    else:
-        board[i - 1][j - 1] = board[i - 1][j] = board[i - 1][j + 1] = 2
-        board[i][j - 1] = board[i][j] = board[i][j + 1] = 2
-        board[i + 1][j - 1] = board[i + 1][j] = board[i + 1][j + 1] = 2
-
-
 def solution(board):
-    answer = 0
-    check_board = [[0 for i in range(len(board))] for i in range(len(board))]
-    if len(board) == 1:
-        return 1 if board[0][0] == 0 else 0
-    for i in range(len(board)):
-        for j in range(len(board)):
+    n = len(board)
+    safe_list = [[True] * n for _ in range(n)]
+    
+    around = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
+    
+    def check(i, j):
+        for x, y in around:
+            if {i + x, j + y} & {-1, n}:
+                continue
+            safe_list[i+x][j+y] = False
+    
+    for i in range(n):
+        for j in range(n):
             if board[i][j] == 1:
-                # 위험지대 표시
-                check_danger(check_board, i, j)
-    for i in check_board:
-        answer += i.count(0)
-    return answer
+                check(i, j)
+    
+    return sum(sum(row) for row in safe_list)
