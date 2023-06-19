f53fc899b43ff3654d72f287542b15c4b0905e52
Charmull <56704985+Charmull@users.noreply.github.com>
2023-06-19 22:30:49 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,10 +1,10 @@
 def solution(dots):
     answer = 0
-    for i in range(len(dots)):
-        for j in range(i + 1, len(dots)):
-            rest = [0, 1, 2, 3]
-            rest.remove(i)
-            rest.remove(j)
-            if abs((dots[i][0] - dots[j][0]) / (dots[i][1] - dots[j][1])) == abs((dots[rest[0]][0] - dots[rest[1]][0]) / (dots[rest[0]][1] - dots[rest[1]][1])):
-                return 1
+    # for i in range(len(dots)):
+    for j in range(1, len(dots)):
+        rest = [0, 1, 2, 3]
+        rest.remove(0)
+        rest.remove(j)
+        if abs((dots[0][0] - dots[j][0]) / (dots[0][1] - dots[j][1])) == abs((dots[rest[0]][0] - dots[rest[1]][0]) / (dots[rest[0]][1] - dots[rest[1]][1])):
+            return 1
     return answer
