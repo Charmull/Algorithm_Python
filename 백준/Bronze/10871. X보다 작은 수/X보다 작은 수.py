93c42911ff8c525fb4238d07f07685a698774969
Charmull <527wlals@gmail.com>
2023-07-14 13:48:16 +0900

    [Bronze V] Title: X보다 작은 수, 풀이 방법 추가

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -4,3 +4,7 @@ result = []
 for v in l:
     if v < x: result.append(v)
 print(' '.join(map(str, result)))
+
+# n, x = map(int, input().split())
+# answer = ' '.join([i for i in input().split() if int(i) < x])
+# print(answer, end='')
