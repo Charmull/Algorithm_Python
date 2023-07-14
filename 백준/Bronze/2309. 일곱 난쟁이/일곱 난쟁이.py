b41912ed54c70669e6c297ebca463eb9661509ef
Charmull <527wlals@gmail.com>
2023-07-14 15:41:14 +0900

    [Bronze I] Title: 일곱 난쟁이, 풀이 방법 추가

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,3 +1,4 @@
+# 풀이 1
 height_list = []
 for i in range(9):
     height_list.append(int(input()))
@@ -14,3 +15,17 @@ del height_list[del_idx[0]]
 del height_list[del_idx[1] - 1]
 
 print('\n'.join(map(str, sorted(height_list))))
+
+
+# # 풀이 2
+# from itertools import combinations
+# heights = []
+
+# for _ in range(9):
+#     heights.append(int(input()))
+
+# for combi in combinations(heights, 7):
+#   if sum(combi) == 100:
+#     for height in sorted(combi):
+#         print(height)
+#     break
