49fd6dfd8aa7d830b18ee34b7c53c3f36bba0a49
Charmull <56704985+Charmull@users.noreply.github.com>
2023-07-15 18:39:38 +0900

    [Silver III] Title: 두 수의 합, Time: 100 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,11 +1,16 @@
 n = int(input())
 nums = list(map(int, input().split()))
 x = int(input())
-occur = [0 for _ in range(x + 1)]
+nums.sort()
+left, right = 0, n - 1
 
 result = 0
-for num in nums:
-    if x - num <= 0: continue
-    if occur[x - num] == 1: result += 1
-    occur[num] = 1
+while left < right:
+    temp = nums[left] + nums[right]
+    if temp == x:
+        result += 1
+    if temp < x:
+        left += 1
+        continue
+    right -= 1
 print(result)
