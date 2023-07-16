5db358f8990048ac98e3c91b55fcbeb59c6c81e3
Charmull <56704985+Charmull@users.noreply.github.com>
2023-07-16 21:49:43 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,10 +1,5 @@
 n = int(input())
 nums = list(map(int, input().split()))
 target = int(input())
-result = 0
 
-for num in nums:
-    if target == num:
-        result += 1
-
-print(result)
+print(nums.count(target))
