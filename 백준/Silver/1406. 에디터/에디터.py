f73293f8b0163ccedc2c265e51ccead24b837b0a
Charmull <527wlals@gmail.com>
2023-07-17 16:28:23 +0900

    [Silver II] Title: 에디터, 풀이 방법 추가

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,3 +1,4 @@
+# 풀이 2
 import sys
 
 st1 = list(sys.stdin.readline().rstrip())
@@ -16,3 +17,22 @@ for _ in range(int(sys.stdin.readline())):
 
 st1.extend(reversed(st2))
 print(''.join(st1))
+
+
+# # 풀이 1
+# text = input()
+# cursor = len(text)
+# for _ in range(int(input())):
+#     command = input()
+#     if command == "L":
+#         if cursor != 0: cursor -= 1
+#     elif command == "D":
+#         if cursor != len(text): cursor += 1
+#     elif command == "B":
+#         if cursor != 0:
+#             text = text[:cursor - 1] + text[cursor:]
+#             cursor -= 1
+#     else:
+#         text = text[:cursor] + command[2] + text[cursor:]
+#         if cursor != len(text): cursor += 1
+# print(text)
