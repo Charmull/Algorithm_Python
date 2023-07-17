8b1d1296c30cf41e3bbf2cb26657ce02e4244ea8
Charmull <56704985+Charmull@users.noreply.github.com>
2023-07-17 21:27:58 +0900

    [Silver II] Title: 스택 수열, Time: 3920 ms, Memory: 37088 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,23 +1,16 @@
-n = int(input())
-lst = []
-stack = []
-for _ in range(n):
-    lst.append(int(input()))
+def solution():
+    n = int(input())
+    nums = [int(input()) for _ in range(n)]
+    stack, answer = [], []
+    now = 1
+    for num in nums:
+        while now <= num:
+            stack.append(now)
+            answer.append('+')
+            now += 1
+        if stack.pop() != num:
+            return 'NO'
+        answer.append('-')
+    return '\n'.join(answer)
 
-tmp = 1
-result = []
-while tmp <= n:
-    if len(stack) != 0 and stack[len(stack) - 1] == lst[0]:
-        stack.pop()
-        result.append('-')
-        del lst[0]
-    else:
-        stack.append(tmp)
-        result.append('+')
-        tmp += 1
-
-if tmp > n and stack == list(reversed(lst)):
-    result += ['-' for _ in stack]
-    print('\n'.join(result))
-else:
-    print("NO")
+print(solution())
