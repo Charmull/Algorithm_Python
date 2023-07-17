16743a2e38a67e9e6d5521276b036ddb63b5b5f0
Charmull <527wlals@gmail.com>
2023-07-17 21:32:13 +0900

    [Silver II] Title: 스택 수열, 풀이 방법 추가

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,3 +1,4 @@
+# 풀이 2 / 37088KB, 3920ms
 def solution():
     n = int(input())
     nums = [int(input()) for _ in range(n)]
@@ -14,3 +15,29 @@ def solution():
     return '\n'.join(answer)
 
 print(solution())
+
+
+# # 풀이 1 / 36944KB, 5648ms
+# n = int(input())
+# lst = []
+# stack = []
+# for _ in range(n):
+#     lst.append(int(input()))
+
+# tmp = 1
+# result = []
+# while tmp <= n:
+#     if len(stack) != 0 and stack[len(stack) - 1] == lst[0]:
+#         stack.pop()
+#         result.append('-')
+#         del lst[0]
+#     else:
+#         stack.append(tmp)
+#         result.append('+')
+#         tmp += 1
+
+# if tmp > n and stack == list(reversed(lst)):
+#     result += ['-' for _ in stack]
+#     print('\n'.join(result))
+# else:
+#     print("NO")
