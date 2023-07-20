ad797025dfb69376278273dae6431a8ab871e53c
Charmull <527wlals@gmail.com>
2023-07-20 17:08:19 +0900

    [Gold V] Title: 탑, 풀이 방법 추가

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -15,3 +15,19 @@ for i in range(n):
     stack.append([i, heights[i]])
 
 print(' '.join(map(str, result)))
+
+
+# # 풀이 1 (시간초과)
+# import sys
+
+# n = int(sys.stdin.readline())
+# heights = list(map(int, sys.stdin.readline().split()))
+# result = [0 for _ in range(n)]
+
+# for i in range(n - 1, 0, -1):
+#     for j in range(i - 1, 0, -1):
+#         if heights[i] <= heights[j]:
+#             result[i] = j + 1
+#             break
+
+# print(' '.join(map(str, result)))
