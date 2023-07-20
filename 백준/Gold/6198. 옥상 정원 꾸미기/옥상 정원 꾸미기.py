e46fbb7b5f348b5255b0facf4cdb701541d22d16
Charmull <527wlals@gmail.com>
2023-07-20 18:48:55 +0900

    [Gold V] Title: 옥상 정원 꾸미기, 풀이 방법 추가

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,3 +1,4 @@
+# 풀이 1 / 해당 빌딩이 볼 수 있는 빌딩 수를 구하는 방법
 import sys
 
 n = int(sys.stdin.readline())
@@ -15,4 +16,21 @@ for i in range(n - 1, -1, -1):
             break
     stack.append((i, heights[i], current_can_see))
     answer += current_can_see
+print(answer)
+
+
+# 풀이 2 / 해당 빌딩을 볼 수 있는 빌딩 수를 구하는 방법
+import sys
+
+n = int(sys.stdin.readline())
+heights = [int(sys.stdin.readline()) for _ in range(n)]
+answer = 0
+stack = []
+
+for i in range(n):
+    x = heights[i]
+    while stack and stack[-1] <= x:
+        stack.pop()
+    answer += len(stack)
+    stack.append(x)
 print(answer)
