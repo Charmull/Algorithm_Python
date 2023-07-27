979fdb77193404970fcd9ed59455be183f31fa2d
Charmull <527wlals@gmail.com>
2023-07-27 21:15:56 +0900

    [Silver II] Title: 쇠막대기, 풀이 방법 추가

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -20,3 +20,26 @@ for v in str:
         else:
             total += 1
 print(total)
+
+
+# # 풀이 2
+# import sys
+
+# parentheses = sys.stdin.readline().strip()
+# stack = []
+# cutting_count = 0
+
+# for ele in parentheses:
+#     if ele == '(':
+#         stack.append(ele)
+#         last = ele
+#     else:
+#         if last == '(':
+#             stack.pop()
+#             cutting_count += len(stack)
+#             last = ele
+#         else:
+#             stack.pop()
+#             cutting_count += 1
+
+# print(cutting_count)
