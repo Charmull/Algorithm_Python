8f3de70f496778cff45d3f970de3d957ec54b2e4
Charmull <527wlals@gmail.com>
2023-07-28 23:24:00 +0900

    [Silver I] Title: 괄호의 값, 풀이 방법 추가

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -1,3 +1,4 @@
+# 풀이 2 / 재귀, 스택 사용
 import sys
 
 input_str = sys.stdin.readline().strip()
@@ -54,3 +55,37 @@ def cal(arr):
 
 arr = go(input_str)
 print(cal(arr))
+
+
+# # 풀이 3 / 스택만 사용
+# import sys
+
+# bracket = sys.stdin.readline().strip()
+# length = len(bracket)
+# stack = []
+# num = 1
+# total = 0
+
+# for i in range(length):
+#     b = bracket[i]
+#     pair = '(' if b == ')' else '['
+#     operand = 2 if b == ')' else 3
+
+#     if b == '(':
+#         num *= 2
+#         stack.append(b)
+#     elif b == '[':
+#         num *= 3
+#         stack.append(b)
+#     else:
+#         if not stack or stack[-1] != pair:
+#             total = 0
+#             break
+#         if bracket[i-1] == pair:
+#             total += num
+#         num //= operand
+#         stack.pop()
+
+# if stack:
+#     total = 0
+# print(total)
