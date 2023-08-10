77d20e643dd1a19100d977933852ef7f3ee05c0b
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-11 00:56:29 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -1,39 +1,16 @@
-# 풀이 1
-a, b = input(), input()
-a = sorted(a)
-b = sorted(b)
+import sys
+
+input = sys.stdin.readline
+a = input().strip()
+b = input().strip()
+a_alph_nums = [0] * 26
+b_alph_nums = [0] * 26
+for v in a:
+    a_alph_nums[ord(v) - ord('a')] += 1
+for v in b:
+    b_alph_nums[ord(v) - ord('a')] += 1
+
 count = 0
-while len(a) and len(b):
-    if a[0] != b[0]:
-        if a[0] in b:
-            target_idx = b.index(a[0])
-            b = b[target_idx:]
-            count += target_idx
-        elif b[0] in a:
-            target_idx = a.index(b[0])
-            a = a[target_idx:]
-            count += target_idx
-        else:
-            a = a[1:]
-            b = b[1:]
-            count += 2
-        continue
-    a = a[1:]
-    b = b[1:]
-
-if a or b:
-    count += len(a) + len(b)
-
+for i in range(26):
+    count += abs(a_alph_nums[i] - b_alph_nums[i])
 print(count)
-
-
-# # 풀이 2
-# a = input()
-# b = input()
-
-# for i in a:
-#   if i in b:
-#     a = a.replace(i,'',1)
-#     b = b.replace(i,'',1)
-
-# print(len(a) + len(b))
