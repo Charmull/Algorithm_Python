ffdf1482b21a5d6338de124277b796a62921ad34
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-12 00:36:22 +0900

    [Bronze II] Title: 방 배정, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
 l = [[0, 0] for _ in range(6)]
-for i in range(n):
+for _ in range(n):
     s, y = map(int, input().split())
     l[y - 1][s] += 1
 room = 0
 for n1, n2 in l:
-    room += n1 // 2 + 1 if n1 % 2 else n1 //2
-    room += n2 // 2 + 1 if n2 % 2 else n2 //2
+    room += n1 // k + 1 if n1 % 2 else n1 // k
+    room += n2 // k + 1 if n2 % 2 else n2 // k
 print(room)
