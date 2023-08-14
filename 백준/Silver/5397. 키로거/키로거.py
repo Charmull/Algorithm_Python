f23921f41afa1b3375fb4f9a22461a79f4975fd5
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-14 21:55:47 +0900

    [Silver II] Title: 키로거, Time: 980 ms, Memory: 38072 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -1,16 +1,19 @@
-n = int(input())
-for _ in range(n):
-    st1 = []
-    st2 = []
-    command = list(input())
-    for c in command:
-        if c == '-':
-            if st1: st1.pop()
-        elif c == '<':
-            if st1: st2.append(st1.pop())
-        elif c == '>':
-            if st2: st1.append(st2.pop())
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    string = input().strip()
+    left = []
+    right = []
+    for v in string:
+        if v == '-':
+            if left: left.pop()
+        elif v == '<':
+            if left: right.append(left.pop())
+        elif v == '>':
+            if right: left.append(right.pop())
         else:
-            st1.append(c)
-    pw = st1 + list(reversed(st2))
-    print(''.join(pw))
+            left.append(v)
+    right.reverse()
+    print(''.join(left) + ''.join(right))
