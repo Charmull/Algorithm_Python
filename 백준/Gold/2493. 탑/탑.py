0bcf272045bb18c9404a31d1758ab80670dc2168
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-14 23:05:07 +0900

    [Gold V] Title: 탑, Time: 548 ms, Memory: 146380 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,33 +1,23 @@
 import sys
 
-n = int(sys.stdin.readline())
-heights = list(map(int, sys.stdin.readline().split()))
-stack = []
-result = [0] * n
+input = sys.stdin.readline
+n = int(input())
+heights = list(map(int, input().split()))
+group = []
+result = []
 
-for i in range(n):
-    while stack:
-        if stack[-1][1] > heights[i]:
-            result[i] = stack[-1][0] + 1
-            break
-        else:
-            stack.pop()
-    stack.append([i, heights[i]])
+for idx, height in enumerate(heights):
+    is_meet = False
+    while group:
+        if group[-1][1] < height:
+            group.pop()
+            continue
+        result.append(group[-1][0])
+        group.append((idx + 1, height))
+        is_meet = True
+        break
+    if not is_meet:
+        result.append(0)
+        group.append((idx + 1, height))
 
 print(' '.join(map(str, result)))
-
-
-# # 풀이 1 (시간초과)
-# import sys
-
-# n = int(sys.stdin.readline())
-# heights = list(map(int, sys.stdin.readline().split()))
-# result = [0 for _ in range(n)]
-
-# for i in range(n - 1, 0, -1):
-#     for j in range(i - 1, 0, -1):
-#         if heights[i] <= heights[j]:
-#             result[i] = j + 1
-#             break
-
-# print(' '.join(map(str, result)))
