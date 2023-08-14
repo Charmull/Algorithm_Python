7d19befd39a6dae2323a7a0b709401f9fee1cb6f
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-14 22:46:28 +0900

    [Silver II] Title: 스택 수열, Time: 116 ms, Memory: 37480 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,43 +1,20 @@
-# 풀이 2 / 37088KB, 3920ms
-def solution():
-    n = int(input())
-    nums = [int(input()) for _ in range(n)]
-    stack, answer = [], []
-    now = 1
-    for num in nums:
-        while now <= num:
-            stack.append(now)
-            answer.append('+')
-            now += 1
-        if stack.pop() != num:
-            return 'NO'
-        answer.append('-')
-    return '\n'.join(answer)
+import sys
 
-print(solution())
+input = sys.stdin.readline
+n = int(input())
+nums = [int(input()) for _ in range(n)]
+stack = []
+add_num = 1
+result = []
 
+for num in nums:
+    while add_num <= num:
+        stack.append(add_num)
+        add_num += 1
+        result.append("+")
+    if stack.pop(-1) != num:
+        print("NO")
+        sys.exit(0)
+    result.append("-")
 
-# # 풀이 1 / 36944KB, 5648ms
-# n = int(input())
-# lst = []
-# stack = []
-# for _ in range(n):
-#     lst.append(int(input()))
-
-# tmp = 1
-# result = []
-# while tmp <= n:
-#     if len(stack) != 0 and stack[len(stack) - 1] == lst[0]:
-#         stack.pop()
-#         result.append('-')
-#         del lst[0]
-#     else:
-#         stack.append(tmp)
-#         result.append('+')
-#         tmp += 1
-
-# if tmp > n and stack == list(reversed(lst)):
-#     result += ['-' for _ in stack]
-#     print('\n'.join(result))
-# else:
-#     print("NO")
+print('\n'.join(result))
