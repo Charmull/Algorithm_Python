ab2ef892a9b1e9f0803d645ca49afc6adf7e653f
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-14 22:24:50 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,23 +1,23 @@
 import sys
 
-n = int(sys.stdin.readline())
-dat = []
-pos = 0
-
+input = sys.stdin.readline
+n = int(input())
+stack = []
 for _ in range(n):
-    command = sys.stdin.readline().split()
-    if command[0] == 'pop':
-        if pos == 0:
-            print(-1)
-        else:
-            print(dat.pop())
-            pos -= 1
-    elif command[0] == 'size':
-        print(pos)
-    elif command[0] == 'empty':
-        print(1 if pos == 0 else 0)
-    elif command[0] == 'top':
-        print(-1 if pos == 0 else dat[pos - 1])
-    else:
-        dat.append(int(command[1]))
-        pos += 1
+    commands = input().split()
+    if commands[0] == 'push':
+        stack.append(commands[1])
+    elif commands[0] == 'pop':
+        if stack:
+            print(stack.pop())
+            continue
+        print(-1)
+    elif commands[0] == 'size':
+        print(len(stack))
+    elif commands[0] == 'empty':
+        print(0 if stack else 1)
+    elif commands[0] == 'top':
+        if stack:
+            print(stack[-1])
+            continue
+        print(-1)
