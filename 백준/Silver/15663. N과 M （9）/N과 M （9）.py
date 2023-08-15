268b99278f1b48c1dfa9f81f721a9296d6223c1e
Charmull <527wlals@gmail.com>
2023-08-15 20:24:39 +0900

    [Silver II] Title: N과 M (9), 풀이 방법 추가

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,3 +1,4 @@
+# 풀이 2
 import sys
 
 input = sys.stdin.readline
@@ -24,3 +25,33 @@ def dfs():
         appended[i] = 0
         
 dfs()
+
+
+# # 풀이 1
+# import sys
+
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# nums = list(map(int, input().split()))
+# nums.sort()
+# l = []
+# appended = [0] * n
+# results = set()
+
+# def dfs():
+#     if len(l) == m:
+#         if tuple(l) not in results:
+#             print(' '.join(map(str, l)))
+#             results.add(tuple(l))
+#         return
+    
+#     for i in range(n):
+#         if appended[i]:
+#             continue
+#         l.append(nums[i])
+#         appended[i] = 1
+#         dfs()
+#         l.pop()
+#         appended[i] = 0
+        
+# dfs()
