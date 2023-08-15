e6a10141edfe8211e5235017dac5152f13cca28b
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-15 20:47:06 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+l = []
+
+def dfs(k):
+    if len(l) == m:
+        print(' '.join(map(str, l)))
+        return
+    
+    prev = 0
+    for i in range(k, n):
+        if prev == nums[i]:
+            continue
+        l.append(nums[i])
+        prev = nums[i]
+        dfs(i)
+        l.pop()
+
+dfs(0)
