7c63c3ea4cdb787dfa0d909359db3a0c3eb375e7
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-15 19:09:47 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+l = []
+
+def dfs(k):
+    if len(l) == m:
+        print(' '.join(map(str, l)))
+        return
+    
+    for i in range(k, n + 1):
+        l.append(i)
+        dfs(i)
+        l.pop()
+        
+dfs(1)
