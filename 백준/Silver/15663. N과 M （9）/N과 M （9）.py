4b4f282d74696a58aa89d3e00748b82a60fc84e3
Charmull <56704985+Charmull@users.noreply.github.com>
2023-08-15 20:18:36 +0900

    [Silver II] Title: N과 M (9), Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -6,20 +6,19 @@ nums = list(map(int, input().split()))
 nums.sort()
 l = []
 appended = [0] * n
-results = set()
 
 def dfs():
     if len(l) == m:
-        if tuple(l) not in results:
-            print(' '.join(map(str, l)))
-            results.add(tuple(l))
+        print(' '.join(map(str, l)))
         return
     
+    prev = 0
     for i in range(n):
-        if appended[i]:
+        if appended[i] or prev == nums[i]:
             continue
         l.append(nums[i])
         appended[i] = 1
+        prev = nums[i]
         dfs()
         l.pop()
         appended[i] = 0
