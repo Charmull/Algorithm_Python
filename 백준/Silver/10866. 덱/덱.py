653cfe7957f7617d89091d1f7f4e78fba5c91bcc
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-11 22:43:48 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,24 +1,25 @@
 import sys
 from collections import deque
 
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
+n = int(input())
 deq = deque()
 
 for _ in range(n):
-    command = sys.stdin.readline().split()
-    if command[0] == 'push_front':
-        deq.appendleft(command[1])
-    elif command[0] == 'push_back':
-        deq.append(command[1])
-    elif command[0] == 'pop_front':
-        print(-1 if len(deq) == 0 else deq.popleft())
-    elif command[0] == 'pop_back':
-        print(-1 if len(deq) == 0 else deq.pop())
-    elif command[0] == 'size':
+    commands = input().split()
+    if commands[0] == 'push_front':
+        deq.appendleft(commands[1])
+    elif commands[0] == 'push_back':
+        deq.append(commands[1])
+    elif commands[0] == 'pop_front':
+        print(-1 if not deq else deq.popleft())
+    elif commands[0] == 'pop_back':
+        print(-1 if not deq else deq.pop())
+    elif commands[0] == 'size':
         print(len(deq))
-    elif command[0] == 'empty':
+    elif commands[0] == 'empty':
         print(0 if deq else 1)
-    elif command[0] == 'front':
-        print(deq[0] if deq else -1)
-    elif command[0] == 'back':
-        print(deq[-1] if deq else -1)
+    elif commands[0] == 'front':
+        print(-1 if not deq else deq[0])
+    elif commands[0] == 'back':
+        print(-1 if not deq else deq[-1])
