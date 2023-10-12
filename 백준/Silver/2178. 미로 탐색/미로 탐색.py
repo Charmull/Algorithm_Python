a5b5ccc26dbbff69ec817a72603e5697f8190925
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-12 22:31:06 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,31 +3,29 @@ from collections import deque
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
-graph = [list(input().strip()) for _ in range(n)]
-dist = [[-1] * m for _ in range(n)]
-result = n * m
+maze = [list(map(int, input().strip())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
 
 dy = (-1, 0, 0, 1)
 dx = (0, -1, 1, 0)
 def bfs(start_row, start_col):
     deq = deque([[start_row, start_col]])
-    dist[start_row][start_col] = 1
+    
+    visited[start_row][start_col] = 1
     
     while deq:
         y, x = deq.popleft()
-        current_dist = dist[y][x]
-        if x == m - 1 and y == n - 1:
-            result = current_dist
-            print(result)
-            break
+        current_count = visited[y][x]
+        if y == n - 1 and x == m - 1:
+            return current_count
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             if ny < 0 or nx < 0 or ny >= n or nx >= m:
                 continue
-            if dist[ny][nx] != -1 or graph[ny][nx] == '0':
+            if not maze[ny][nx] or visited[ny][nx]:
                 continue
             deq.append([ny, nx])
-            dist[ny][nx] = current_dist + 1
-
-bfs(0, 0)
+            visited[ny][nx] = current_count + 1
+            
+print(bfs(0, 0))
