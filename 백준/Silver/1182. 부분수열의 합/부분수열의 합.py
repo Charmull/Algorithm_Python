cd777a673446fe6ab8f91d2869df802b37fcda81
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-15 01:15:51 +0900

    [Silver II] Title: 부분수열의 합, Time: 476 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,25 +2,17 @@ import sys
 
 input = sys.stdin.readline
 n, s = map(int, input().split())
-l = list(map(int, input().split()))
+nums = list(map(int, input().split()))
 log = []
-appended = [0] * n
-result = 0
+count = [0]
 
-def make_s(k):
-    global result
-
+def dfs(k):
     if log and sum(log) == s:
-        result += 1
-    
+        count[0] += 1
     for i in range(k, n):
-        if appended[i]:
-            continue
-        log.append(l[i])
-        appended[i] = 1
-        make_s(i + 1)
+        log.append(nums[i])
+        dfs(i + 1)
         log.pop()
-        appended[i] -= 1
-
-make_s(0)
-print(result)
+        
+dfs(0)
+print(count[0])
