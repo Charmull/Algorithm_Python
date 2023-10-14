286df0d6b2c997e37aa00fb9e849e38b3b069a23
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-15 00:11:07 +0900

    [Gold IV] Title: N-Queen, Time: 29360 ms, Memory: 212456 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,28 @@
-import sys
-n = int(sys.stdin.readline().rstrip())
+n = int(input())
 
-ans = 0
-row = [0] * n
+result = [0]
+log = []
 
-def is_promising(x):
-    for i in range(x):
-        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+def is_valid(nx, ny):
+    for x, y in log:
+        if x == nx or y == ny:
+            return False
+        if x - y == nx - ny:
+            return False
+        if x + y == nx + ny:
             return False
     return True
 
-def n_queens(x):
-    global ans
-
-    if x == n:
-        ans += 1
+def dfs(row):
+    if len(log) == n:
+        result[0] += 1
         return
-    for i in range(n):
-        # [x, i]에 퀸을 놓겠다.
-        row[x] = i
-        if is_promising(x):
-            n_queens(x + 1)
+    
+    for col in range(n):
+        if is_valid(row, col):
+            log.append((row, col))
+            dfs(row + 1)
+            log.pop()
 
-n_queens(0)
-print(ans)
+dfs(0)
+print(result[0])
