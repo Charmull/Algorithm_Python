db86a6db8430c7065f06312ca4fb875637912e34
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-14 17:58:54 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -3,24 +3,29 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
-count_white = 0
-count_blue = 0
+count = [0, 0]
 
-def count(start_row, start_col, n):
-    global count_white, count_blue
-    
-    prev = matrix[start_row][start_col]
-    for y in range(start_row, start_row + n):
-        for x in range(start_col, start_col + n):
-            if prev != matrix[y][x]:
-                for i in range(2):
-                    for j in range(2):
-                        count(start_row + i * n // 2, start_col + j * n // 2, n // 2)
-                return 0
-    if prev:
-        count_blue += 1
+def count_num(k, row, col):
+    prev = matrix[row][col]
+    flag = False
+    for i in range(row, row + k):
+        for j in range(col, col + k):
+            if prev != matrix[i][j]:
+                flag = True
+                break
+        if flag:
+            break
+            
+    if flag:
+        half = k // 2
+        for i in (row, row + half):
+            for j in (col, col + half):
+                count_num(half, i, j)
     else:
-        count_white += 1
-        
-count(0, 0, n)
-print(count_white, count_blue, sep="\n")
+        if prev:
+            count[1] += 1
+        else:
+            count[0] += 1
+            
+count_num(n, 0, 0)
+print(count[0], count[1], sep='\n')
