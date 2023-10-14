f60e6ea2406d483f2d859b1b86b4f02491b70889
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-14 19:52:21 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,7 +2,6 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 l = []
-appended = [0] * (n + 1)
 
 def dfs(k):
     if len(l) == m:
@@ -10,12 +9,8 @@ def dfs(k):
         return
     
     for i in range(k, n + 1):
-        if appended[i]:
-            continue
         l.append(i)
-        appended[i] = 1
         dfs(i + 1)
         l.pop()
-        appended[i] = 0
 
 dfs(1)
