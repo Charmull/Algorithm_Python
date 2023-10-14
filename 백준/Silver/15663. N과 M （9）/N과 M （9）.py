accb03160b924b2771e4188fac92a8a25c110c84
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-14 23:07:34 +0900

    [Silver II] Title: N과 M (9), Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,57 +1,26 @@
-# 풀이 2
 import sys
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-l = []
+log = []
 appended = [0] * n
 
 def dfs():
-    if len(l) == m:
-        print(' '.join(map(str, l)))
+    if len(log) == m:
+        print(' '.join(map(str, log)))
         return
     
     prev = 0
     for i in range(n):
         if appended[i] or prev == nums[i]:
             continue
-        l.append(nums[i])
+        log.append(nums[i])
         appended[i] = 1
         prev = nums[i]
         dfs()
-        l.pop()
+        log.pop()
         appended[i] = 0
         
 dfs()
-
-
-# # 풀이 1
-# import sys
-
-# input = sys.stdin.readline
-# n, m = map(int, input().split())
-# nums = list(map(int, input().split()))
-# nums.sort()
-# l = []
-# appended = [0] * n
-# results = set()
-
-# def dfs():
-#     if len(l) == m:
-#         if tuple(l) not in results:
-#             print(' '.join(map(str, l)))
-#             results.add(tuple(l))
-#         return
-    
-#     for i in range(n):
-#         if appended[i]:
-#             continue
-#         l.append(nums[i])
-#         appended[i] = 1
-#         dfs()
-#         l.pop()
-#         appended[i] = 0
-        
-# dfs()
