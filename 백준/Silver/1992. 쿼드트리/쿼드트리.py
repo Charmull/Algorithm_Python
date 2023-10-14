90f24d48db9bbdc8b253aca2621a6fb41882b2ec
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-14 18:39:32 +0900

    [Silver I] Title: 쿼드트리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -3,26 +3,21 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 matrix = [list(map(int, list(input().strip()))) for _ in range(n)]
-result = ""
+result = []
 
-def quad_tree(start_row, start_col, n):
-    global result
-    
-    prev = matrix[start_row][start_col]
-    for row in range(start_row, start_row + n):
-        for col in range(start_col, start_col + n):
-            if prev != matrix[row][col]:
-                result += "("
-                for i in range(2):
-                    for j in range(2):
-                        quad_tree(start_row + i * n // 2, start_col + j * n // 2, n // 2)
-                result += ")"
-                return 0
+def quad_tree(k, row, col):
+    prev = matrix[row][col]
+    for i in range(row, row + k):
+        for j in range(col, col + k):
+            if prev != matrix[i][j]:
+                result.append('(')
+                for nrow in (row, row + k // 2):
+                    for ncol in (col, col + k // 2):
+                        quad_tree(k // 2, nrow, ncol)
+                result.append(')')
+                return
                 
-    if prev:
-        result += "1"
-    else:
-        result += "0"
-        
-quad_tree(0, 0, n)
-print(result)
+    result.append(prev)
+    
+quad_tree(n, 0, 0)
+print(''.join(map(str, result)))
