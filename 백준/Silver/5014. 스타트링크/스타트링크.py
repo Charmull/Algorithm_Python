d418885871523dd2f278b77d42928da7249a5ab9
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-20 20:41:46 +0900

    [Silver I] Title: 스타트링크, Time: 596 ms, Memory: 79692 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,24 +1,23 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
-f, s, g, u, d = map(int, input().split())
-deq = deque([s])
-dist = [-1] * (f + 1)
-dist[s] = 0
+def bfs(v):
+    q = deque([v])
+    visited[v] = 1
+    while q:
+        v = q.popleft()
+        if v == G:
+            return count[G]
+        for i in (v+U, v-D):
+            if 0 < i <= F and not visited[i]:
+                visited[i] = 1
+                count[i] = count[v] + 1
+                q.append(i)
+    if count[G] == 0:
+        return "use the stairs"
 
-while deq:
-    current_floor = deq.popleft()
-    current_dist = dist[current_floor]
-    if current_floor == g:
-        print(current_dist)
-        sys.exit(0)
-    for v in [current_floor + u, current_floor - d]:
-        next_floor = v
-        if next_floor <= 0 or next_floor > f:
-            continue
-        if dist[next_floor] != -1:
-            continue
-        deq.append(next_floor)
-        dist[next_floor] = current_dist + 1
-print("use the stairs")
+input = sys.stdin.readline
+F, S, G, U, D = map(int, input().split())
+visited = [0 for i in range(F+1)]
+count = [0 for i in range(F+1)]
+print(bfs(S))
