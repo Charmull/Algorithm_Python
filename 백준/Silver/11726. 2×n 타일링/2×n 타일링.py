dac495177a6115eccdb570dd035198ef6379c303
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-28 07:34:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+if n == 1:
+    print(1)
+    sys.exit(0)
+
+d = [0] * (n + 1)
+d[1] = 1
+d[2] = 2
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+print(d[n] % 10007)
