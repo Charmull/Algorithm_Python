5a986013cde22a9d0fa8e8f516e73935a83935f6
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-28 12:43:49 +0900

    [Silver I] Title: 1로 만들기 2, Time: 580 ms, Memory: 78492 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,25 +1,35 @@
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
-dist = [0] * (n + 1)
-def bfs(start):
-    deq = deque([[start]])
-    dist[start] = 1
-    
-    while deq:
-        route = deq.popleft()
-        x = route[-1]
-        curr_cnt = dist[x]
-        if x == n:
-            print(curr_cnt - 1)
-            print(' '.join(map(str, route[::-1])))
-        for nx in (x * 3, x * 2, x + 1):
-            if nx < 1 or nx > n:
-                continue
-            if dist[nx]:
-                continue
-            deq.append(route + [nx])
-            dist[nx] = curr_cnt + 1
-            
-bfs(1)
+
+if n == 1:
+    print(0)
+    print(1)
+    sys.exit(0)
+elif n < 3:
+    print(1)
+    print(2, 1)
+    sys.exit(0)
+
+d = [0] * (n + 1)
+pre = [0] * (n + 1)
+d[2] = 1
+pre[2] = 1
+d[3] = 1
+pre[3] = 1
+
+for i in range(4, n + 1):
+    d[i] = d[i - 1] + 1
+    pre[i] = i - 1
+    if not i % 2 and d[i] > d[i // 2] + 1:
+        d[i] = d[i // 2] + 1
+        pre[i] = i // 2
+    if not i % 3 and d[i] > d[i // 3] + 1:
+        d[i] = d[i // 3] + 1
+        pre[i] = i // 3
+        
+print(d[n])
+curr = n
+while curr:
+    print(curr, end=' ')
+    curr = pre[curr]
