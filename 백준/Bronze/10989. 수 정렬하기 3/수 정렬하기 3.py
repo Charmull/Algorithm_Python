586f5ee5537f5f5a70dda6763481d0f8d68058b9
Charmull <56704985+Charmull@users.noreply.github.com>
2023-10-29 18:37:24 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9312 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+cnts = [0] * 10001
+for _ in range(n):
+    cnts[int(input())] += 1
+    
+for i in range(10001):
+    while cnts[i]:
+        print(i)
+        cnts[i] -= 1
