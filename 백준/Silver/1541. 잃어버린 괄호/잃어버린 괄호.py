4ff3304b3472db91b3c5076e8513d1851ec54eb8
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-01 14:44:30 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -5,7 +5,6 @@ arr = list(input())
 result = 0
 prev = '+'
 is_minus = False
-minus_stack = 0
 for i in range(len(arr)):
     if arr[i] >= '0' and arr[i] <= '9':
         if prev != '-' and prev != '+':
@@ -14,21 +13,20 @@ for i in range(len(arr)):
             prev = int(arr[i])
     elif arr[i] == '-':
         if is_minus:
-            result -= (minus_stack + prev)
-            minus_stack = 0
+            result -= prev
         if not is_minus:
             result += prev
             is_minus = True
         prev = '-'
     elif arr[i] == '+':
         if is_minus:
-            minus_stack += prev
+            result -= prev
         if not is_minus:
             result += prev
         prev = '+'
 
 if is_minus:
-    result -= (minus_stack + prev)
+    result -= prev
 else:
     result += prev
 print(result)
