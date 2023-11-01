193e1a6770506e65130186b4600434c8d8e91564
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-01 21:57:29 +0900

    [Silver III] Title: 소수 구하기, Time: 300 ms, Memory: 46348 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -5,7 +5,7 @@ primes = []
 state = [1] * (n + 1)
 state[0] = 0
 state[1] = 0
-for i in range(2, n + 1):
+for i in range(2, int(n ** (1/2)) + 1):
     if not state[i]:
         continue
     for j in range(i * i, n + 1, i):
