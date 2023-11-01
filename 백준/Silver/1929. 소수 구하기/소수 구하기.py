29f9cbbfbff216b4df5bf60323575d3a31f8ffac
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-01 21:56:34 +0900

    [Silver III] Title: 소수 구하기, Time: 380 ms, Memory: 46348 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -8,7 +8,7 @@ state[1] = 0
 for i in range(2, n + 1):
     if not state[i]:
         continue
-    for j in range(i * 2, n + 1, i):
+    for j in range(i * i, n + 1, i):
         state[j] = 0
 
 for i in range(2, n + 1):
