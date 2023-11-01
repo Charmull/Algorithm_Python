2555facc44e9da2c97ba7d145700598894e31067
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-01 21:55:42 +0900

    [Silver III] Title: 소수 구하기, Time: 420 ms, Memory: 46348 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,18 @@
+import sys
+
+m, n = map(int, sys.stdin.readline().split())
+primes = []
+state = [1] * (n + 1)
+state[0] = 0
+state[1] = 0
+for i in range(2, n + 1):
+    if not state[i]:
+        continue
+    for j in range(i * 2, n + 1, i):
+        state[j] = 0
+
+for i in range(2, n + 1):
+    if state[i] and i >= m:
+        primes.append(i)
+        
+print('\n'.join(map(str, primes)))
