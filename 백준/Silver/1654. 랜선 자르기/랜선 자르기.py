5ace26f21167058badc16dc01ab9e400a5f5333a
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-02 22:37:44 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+k, n = map(int, input().split())
+arr = [int(input()) for _ in range(k)]
+
+def solve(x):
+    global k, n
+    cur = 0
+    for i  in range(k):
+        cur += arr[i] // x
+    return cur >= n
+
+st = 1
+en = 2 ** 31 - 1
+while st < en:
+    mid = (st + en + 1) // 2
+    if solve(mid):
+        st = mid
+    else:
+        en = mid - 1
+
+print(st)
