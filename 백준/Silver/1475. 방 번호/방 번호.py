a611b49e72ea9f294a390351c40758fa6edbc2df
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-10 18:39:00 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,6 +1,19 @@
-n = input().replace('9', '6')
-count = [0 for _ in range(10)]
-for v in n:
-    count[int(v)] += 1
-count[6] = count[6] // 2 if count[6] % 2 == 0 else count[6] // 2 + 1
-print(max(count))
+import sys
+
+n = int(sys.stdin.readline())
+arr = [0] * 9
+while n:
+    if n % 10 == 9:
+        arr[6] += 1
+    else:
+        arr[n % 10] += 1
+    n //= 10
+
+count = 0
+for i in range(9):
+    num = arr[i]
+    if i == 6:
+        num = arr[i] // 2 if not arr[i] % 2 else arr[i] // 2 + 1
+    count = max(count, num)
+
+print(count)
