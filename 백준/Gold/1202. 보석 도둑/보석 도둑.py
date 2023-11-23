4c9278854b504a06227e8270047879b4f731b6c3
Charmull <56704985+Charmull@users.noreply.github.com>
2023-11-23 22:24:33 +0900

    [Gold II] Title: 보석 도둑, Time: 1676 ms, Memory: 165052 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -0,0 +1,20 @@
+import heapq
+
+n, k = map(int, input().split())
+jewel_data = [tuple(map(int, input().split())) for _ in range(n)]
+bag_data = [int(input()) for _ in range(k)]
+
+jewel_data.sort(reverse=True)
+bag_data.sort()
+
+h = []
+
+result = 0
+for c in bag_data:
+    while jewel_data and jewel_data[-1][0] <= c:
+        jewel = jewel_data.pop()
+        heapq.heappush(h, -jewel[1])
+    if h:
+        result += -heapq.heappop(h)
+
+print(result)
