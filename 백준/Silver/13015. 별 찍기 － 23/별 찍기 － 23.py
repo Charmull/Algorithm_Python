f61d79390239bca8407581c26be3da5dda565a8e
Charmull <56704985+Charmull@users.noreply.github.com>
2023-12-26 21:15:12 +0900

    [Silver IV] Title: 별 찍기 - 23, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13015. 별 찍기 － 23/별 찍기 － 23.py
+++ 백준/Silver/13015. 별 찍기 － 23/별 찍기 － 23.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    if i == 1 or i == 2 * n:
+    if i == 1:
         print("*" * n + " " * (2 * (n - 1) - 1) + "*" * n)
     elif i != n:
         print(" " * (i - 1) + "*" + " " * (n - 2) + "*" + " " *
@@ -10,8 +10,8 @@ for i in range(1, n + 1):
         print(" " * (i - 1) + "*" + " " * (n - 2) + "*" +
               " " * (2 * (n - i) - 1) + " " * (n - 2) + "*")
 
-for i in range(n, 0, -1):
-    if i == 1 or i == 2 * n:
+for i in range(n - 1, 0, -1):
+    if i == 1:
         print("*" * n + " " * (2 * (n - 1) - 1) + "*" * n)
     elif i != n:
         print(" " * (i - 1) + "*" + " " * (n - 2) + "*" + " " *
