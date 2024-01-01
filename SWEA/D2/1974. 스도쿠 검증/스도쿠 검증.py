b4e0754ea16eca089886389730065fe86a20f7ae
Charmull <56704985+Charmull@users.noreply.github.com>
2024-01-01 19:50:51 +0900

    [D2] Title: 스도쿠 검증, Time: 152 ms, Memory: 58,800 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,39 +1,35 @@
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    matrix = [list(map(int, input().split())) for _ in range(9)]
-    
-    result = 1
-    for i in range(9):
-        count = [0] * 9
-        if not result:
-            break
-        for j in range(9):
-            count[matrix[i][j] - 1] += 1
-            if count[matrix[i][j] - 1] > 1:
-                result = 0
-                break
-        
-    for j in range(9):
-        count = [0] * 9
-        if not result:
-            break
-        for i in range(9):
-            count[matrix[i][j] - 1] += 1
-            if count[matrix[i][j] - 1] > 1:
-                result = 0
-                break
 
+# 가로, 세로 확인
+def check1(board):
+    for i in range(9):
+        chk1 = [0] * 10
+        chk2 = [0] * 10
+        for j in range(9):
+            # 가로 확인
+            chk1[board[i][j]] += 1
+            if chk1[board[i][j]] >= 2:
+                return 0
+            chk2[board[j][i]] += 1
+            if chk2[board[j][i]] >= 2:
+                return 0
+    return 1
+
+# 3*3 확인
+def check2(board):
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
-            count = [0] * 9
-            if not result:
-                break
-            for k in range(i, i + 3):
-                for m in range(j, j + 3):
-                    count[matrix[k][m] - 1] += 1
-                    if count[matrix[k][m] - 1] > 1:
-                        result = 0
-                        break
-                        
-    print(f'#{test_case}', result)
+            chk = [0] * 10
+            for ii in range(3):
+                for jj in range(3):
+                    chk[board[i + ii][j + jj]] += 1
+                    if chk[board[i + ii][j + jj]] >= 2:
+                        return 0
+    return 1
+
+for test_case in range(1, T + 1):
+    board = [list(map(int, input().split())) for _ in range(9)]
+    if check1(board) == 1 and check2(board) == 1:
+        print(f'#{test_case} 1')
+    else:
+        print(f'#{test_case} 0')
