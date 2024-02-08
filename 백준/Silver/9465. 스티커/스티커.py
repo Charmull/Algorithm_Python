3e22eea63a5836b11b4ff3c8befbf7bde24cf092
Charmull <56704985+Charmull@users.noreply.github.com>
2024-02-08 16:49:51 +0900

    [Silver I] Title: 스티커, Time: 1256 ms, Memory: 58152 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,5 +1,4 @@
 # 1. DP
-# 2. i열일때 해당 칸 넣은 값과 안 넣은 값 비교
 
 import sys
 
@@ -15,9 +14,7 @@ for i in range(t):
         dp[1][1] = max(dp[0][0] + board[1][1], dp[0][1])
 
     for i in range(2, n):
-        dp[i][0] = max(dp[i - 1][1] + board[0][i],
-                       max(dp[i - 2]) + board[0][i], dp[i - 1][0])
-        dp[i][1] = max(dp[i - 1][0] + board[1][i],
-                       max(dp[i - 2]) + board[1][i], dp[i - 1][1])
+        dp[i][0] = max(dp[i - 1][1] + board[0][i], max(dp[i - 2]) + board[0][i])
+        dp[i][1] = max(dp[i - 1][0] + board[1][i], max(dp[i - 2]) + board[1][i])
 
     print(max(dp[n - 1]))
