8c1b4a28f62a489e256c984d6810f145d93462a5
Charmull <56704985+Charmull@users.noreply.github.com>
2024-02-20 17:09:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 5564 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -6,7 +6,6 @@ import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 obj = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
-obj.sort()
 dp = [[0] * (K + 1) for _ in range(N + 1)]
 for i in range(K + 1):
     dp[0][i] = 0
