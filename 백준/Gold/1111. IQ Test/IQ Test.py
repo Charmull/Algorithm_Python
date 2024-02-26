12123bc6d7c8c0dfbda12c5659f9644ab2b05653
Charmull <56704985+Charmull@users.noreply.github.com>
2024-02-26 12:27:17 +0900

    [Gold III] Title: IQ Test, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1111. IQ Test/IQ Test.py
+++ 백준/Gold/1111. IQ Test/IQ Test.py
@@ -1,8 +1,12 @@
+# num[0] * a + b = num[1]
+# num[1] * a + b = num[2]
+# a = (num[1] - num[2]) / (num[0] - num[1])
 import sys
 
 input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
+
 if n == 1:
     print('A')
 elif n == 2:
@@ -11,16 +15,15 @@ elif n == 2:
     else:
         print('A')
 else:
-    if nums[0] - nums[1] == 0:
-        a = 1
+    if nums[0] == nums[1]:
+        a = 0
     else:
         a = (nums[1] - nums[2]) // (nums[0] - nums[1])
     b = nums[1] - nums[0] * a
-    result = nums[-1] * a + b
-
-    for i in range(1, n - 1):
-        if nums[i] * a + b != nums[i + 1]:
-            result = 'B'
+    isCorrect = True
+    for i in range(2, n):
+        if nums[i] != nums[i - 1] * a + b:
+            isCorrect = False
             break
-
-    print(result)
+            
+    print(nums[-1] * a + b if isCorrect else 'B')
