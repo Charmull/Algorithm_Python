024ae596d94a8fe7dd8b88a3d43ff0f029f29bd1
Charmull <56704985+Charmull@users.noreply.github.com>
2024-02-26 12:10:34 +0900

    [Gold V] Title: LCS, Time: 568 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,39 +1,17 @@
-# if A[i] == B[i]:dp[i][j] = dp[i - 1][j - 1] + 1
-# if A[i] != B[i]:dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+# 1. dp[i][j] = 문자열A의 i번째 문자와 문자열B의 j번째 문자까지 고려했을 때 최대 길이
+# 2. dp[i][j] = dp[i - 1][j - 1] + 1 (단, A[i] == B[j])
+#    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) (단, A[i] != B[j])
 import sys
 
 input = sys.stdin.readline
 A = input().strip()
 B = input().strip()
-dp = [[0] * len(A) for _ in range(len(B))]
-if len(A) == 0 or len(B) == 0:
-    print(0)
-    sys.exit(0)
-
-if A[0] == B[0]:
-    dp[0][0] = 1
-flag = dp[0][0]
-for i in range(1, len(B)):
-    if flag:
-        dp[i][0] = 1
-        continue
-    if A[0] == B[i]:
-        dp[i][0] = 1
-        flag = True
-flag = dp[0][0]
-for i in range(1, len(A)):
-    if flag:
-        dp[0][i] = 1
-        continue
-    if A[i] == B[0]:
-        dp[0][i] = 1
-        flag = True
-
-for i in range(1, len(B)):
-    for j in range(1, len(A)):
-        if A[j] == B[i]:
+dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
+        if A[i - 1] == B[j - 1]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
-
-print(dp[len(B) - 1][len(A) - 1])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            
+print(dp[len(A)][len(B)])
