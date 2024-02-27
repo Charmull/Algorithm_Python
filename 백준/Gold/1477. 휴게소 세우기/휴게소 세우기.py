1f83ff6c9d992f99627c158692822aa09a77b1ec
Charmull <56704985+Charmull@users.noreply.github.com>
2024-02-27 22:02:31 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -1,6 +1,10 @@
+# m개의 휴게소를 짓고 난 후에 휴게소가 없는 구간의 최댓값의 최솟값
+# -> 휴게소가 없는 구간의 최댓값이 x면 휴게소 m개를 지을 수 있나?
+# --> x에 대한 휴게소 개수 cnt 값이 단조감소 : 이분탐색
+# --> x일 때 휴게소 개수 cnt가 m보다 크면 : cnt 줄이기, 즉 x 키우기
+# --> x일 때 휴게소 개수 cnt가 m보다 작으면 : cnt 늘리기, 즉 x 줄이기
+# --> x일 때 휴게소 개수 cnt가 m이면 : x가 더 작을때도 같은 cnt일 수도.
 import sys
-from math import ceil
-from collections import defaultdict
 
 input = sys.stdin.readline
 n, m, l = map(int, input().split())
@@ -8,27 +12,20 @@ store = [0] + list(map(int, input().split())) + [l]
 store.sort()
 diff = [store[i + 1] - store[i] for i in range(n + 1)]
 
-dp = [[i] * (n + m + 1) for i in range(l + 1)]
-for i in range(l + 1):
-    for j in range(1, n + m + 1):
-        dp[i][j] = ceil(i / (j + 1))
+def count_store(x):
+    cnt = 0
+    for i in range(n + 1):
+        cnt += (diff[i] - 1) // x
+    return cnt
 
-result = [float('inf')]
-
-memo = [[0] * (m + 1) for _ in range(n + 2)]
-def dfs(m, idx):
-    if idx >= n + 1:
-        return 0
-    if not m:
-        return max(diff[idx:])
-    if not memo[idx][m]:
-        result = float('inf')
-        for i in range(m + 1):
-            result = min(result, max(dfs(m - i, idx + 1), dp[diff[idx]][i]))
-        memo[idx][m] = result
-    else:
-        result = memo[idx][m]
-
-    return result
-
-print(dfs(m, 0))
+st = 1
+en = l - 1
+while st < en:
+    mid = (st + en) // 2
+    cnt = count_store(mid)
+    if cnt > m:
+        st = mid + 1
+    if cnt <= m:
+        en = mid
+        
+print(st)
