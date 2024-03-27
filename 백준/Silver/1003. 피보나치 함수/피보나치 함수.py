d1da6237522b1bd6d6506c0aea78ecf71b741f05
Charmull <56704985+Charmull@users.noreply.github.com>
2024-03-28 00:53:16 +0900

    [Silver III] Title: 피보나치 함수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -2,13 +2,13 @@ import sys
 
 input = sys.stdin.readline
 t = int(input())
-arr = [[0] * 2 for _ in range(41)]
-arr[0] = [1, 0]
-arr[1] = [0, 1]
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
 for i in range(2, 41):
-    arr[i][0] = arr[i - 1][0] + arr[i - 2][0]
-    arr[i][1] = arr[i - 1][1] + arr[i - 2][1]
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
 
 for _ in range(t):
     n = int(input())
-    print(arr[n][0], arr[n][1])
+    print(dp[n][0], dp[n][1])
