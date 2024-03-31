80dbfd04fae0c6befdb7d759f89d8e75924e46b2
Charmull <56704985+Charmull@users.noreply.github.com>
2024-03-31 10:48:57 +0900

    [Gold V] Title: 리모컨, Time: 1432 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,37 +1,40 @@
-# 1. 타겟 채널 번호 자릿수 > abs(현재 채널 번호 - 타겟 채널 번호) 면 후자 출력
-# 2. 브루트포스
+# 1,000,000 * 54 = 54,000,000
 import sys
 
 input = sys.stdin.readline
 N = int(input())
-rm_num = [0] * 10
-rm_cnt = int(input())
-if rm_cnt:
-    tmp = list(map(int, input().split()))
-    for el in tmp:
-        rm_num[el] = 1
-
-
-def isPass(num):
-    str_num = str(num)
-    for el in str_num:
-        if rm_num[int(el)]:
-            return True
-    return False
-
-
-result = abs(100 - N)
-# 타겟 채널 번호 자릿수 > abs(현재 채널 번호 - 타겟 채널 번호) 면 후자 출력
-if len(str(N)) > abs(100 - N):
-    result = abs(100 - N)
-# 고장난 번호키가 10개면
-elif rm_cnt == 10:
-    result = abs(100 - N)
+M = int(input())
+if M:
+    broken = set(map(int, input().split()))
 else:
-    for i in range(1000000):
-        if isPass(i):
-            continue
-        if result > abs(N - i) + len(str(i)):
-            result = abs(N - i) + len(str(i))
+    broken = set()
+if N == 100:
+    print(0)
+    sys.exit()
+
+result = abs(N - 100)
+if M == 10:
+    print(result)
+    sys.exit()
+
+
+def check(num):
+    num_set = set() if num else set([0])
+    num_len = 0 if num else 1
+    tmp = num
+    while tmp:
+        num_set.add(tmp % 10)
+        tmp //= 10
+        num_len += 1
+
+    if (num_set & broken) != set():
+        return False, 0
+    return True, num_len
+
+
+for i in range(0, 1000001):
+    isCanPush, num_len = check(i)
+    if isCanPush:
+        result = min(result, abs(N - i) + num_len)
 
 print(result)
