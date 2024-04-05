958fda0bc02e13d32990123869e402eb3f8ca7d8
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-06 04:24:50 +0900

    [Silver II] Title: 색종이 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,31 +1,40 @@
 import sys
 
 input = sys.stdin.readline
-n = int(input())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-count = [0, 0]
 
-def count_num(k, row, col):
-    prev = matrix[row][col]
-    flag = False
-    for i in range(row, row + k):
-        for j in range(col, col + k):
-            if prev != matrix[i][j]:
-                flag = True
-                break
-        if flag:
-            break
-            
-    if flag:
-        half = k // 2
-        for i in (row, row + half):
-            for j in (col, col + half):
-                count_num(half, i, j)
-    else:
-        if prev:
-            count[1] += 1
+n = int(input())
+origin = [list(map(int, input().split())) for _ in range(n)]
+result = [0, 0]
+
+def re(n, r, c):
+    if n == 1:
+        if origin[r][c] == 0:
+            result[0] += 1
         else:
-            count[0] += 1
-            
-count_num(n, 0, 0)
-print(count[0], count[1], sep='\n')
+            result[1] += 1
+        return
+    color = origin[r][c]
+    isBreak = 0
+    for i in range(r, r + n):
+        for j in range(c, c + n):
+            if color != origin[i][j]:
+                isBreak = 1
+                break
+        if isBreak:
+            break
+
+    if not isBreak:
+        if color == 0:
+            result[0] += 1
+        else:
+            result[1] += 1
+        return
+
+    re(n // 2, r, c)
+    re(n // 2, r, n // 2 + c)
+    re(n // 2, n // 2 + r, c)
+    re(n // 2, n // 2 + r, n // 2 + c)
+
+re(n, 0, 0)
+print(result[0])
+print(result[1])
