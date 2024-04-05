e73f23390edc23e4655f68d12493836b67f1a714
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-06 04:08:22 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,32 +1,21 @@
 import sys
 
 input = sys.stdin.readline
-arr = list(input())
+input_str = input().strip()
 result = 0
-prev = '+'
-is_minus = False
-for i in range(len(arr)):
-    if arr[i] >= '0' and arr[i] <= '9':
-        if prev != '-' and prev != '+':
-            prev = prev * 10 + int(arr[i])
-        else:
-            prev = int(arr[i])
-    elif arr[i] == '-':
-        if is_minus:
-            result -= prev
-        if not is_minus:
-            result += prev
-            is_minus = True
-        prev = '-'
-    elif arr[i] == '+':
-        if is_minus:
-            result -= prev
-        if not is_minus:
-            result += prev
-        prev = '+'
+tmp = ''
+isGetMinus = False
+for el in input_str:
+    if el.isdigit():
+        tmp += el
+        continue
+    if isGetMinus:
+        result -= int(tmp)
+        tmp = ''
+        continue
+    if el == '-':
+        isGetMinus = True
+    result += int(tmp)
+    tmp = ''
 
-if is_minus:
-    result -= prev
-else:
-    result += prev
-print(result)
+print(result - int(tmp) if isGetMinus else result + int(tmp))
