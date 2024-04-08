0abbf007cd0065dd4b6666a5a9dcb2e6a6156442
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-08 23:57:00 +0900

    [Gold III] Title: ACM Craft, Time: 936 ms, Memory: 35672 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,34 +1,31 @@
 import sys
-from collections import defaultdict, deque
+from collections import defaultdict
+sys.setrecursionlimit(1_000_000)
 
 input = sys.stdin.readline
 t = int(input())
 
+
+def solve(target):
+    total_cost[target] = cost[target]
+
+    for p in child_to_parent[target]:
+        if total_cost[p] == -1:
+            solve(p)
+        total_cost[target] = max(
+            total_cost[target], total_cost[p] + cost[target])
+
+    return total_cost[target]
+
+
 for _ in range(t):
     n, k = map(int, input().split())
     cost = [0] + list(map(int, input().split()))
-    parent_to_child = defaultdict(list)
-    parent_cnt = [0] * (n + 1)
+    child_to_parent = defaultdict(list)
     for _ in range(k):
         x, y = map(int, input().split())
-        parent_to_child[x].append(y)
-        parent_cnt[y] += 1
+        child_to_parent[y].append(x)
     target = int(input())
 
-    deq = deque([])
-    total_cost = [0] * (n + 1)
-    for i in range(1, n + 1):
-        if not parent_cnt[i]:
-            deq.append(i)
-
-    while deq:
-        build = deq.popleft()
-        cur_cost = total_cost[build] + cost[build]
-        if build == target:
-            print(cur_cost)
-            break
-        for c in parent_to_child[build]:
-            parent_cnt[c] -= 1
-            total_cost[c] = max(total_cost[c], cur_cost)
-            if not parent_cnt[c]:
-                deq.append(c)
+    total_cost = [-1] * (n + 1)
+    print(solve(target))
