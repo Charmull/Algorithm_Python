d05a091068db1bb5a0ce67054f88e68e628adf8c
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-16 20:06:30 +0900

    [Silver II] Title: 섬의 개수, Time: 76 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dx = [1, -1, 0, 0, 1, -1, 1, -1]
+dy = [0, 0, -1, 1, -1, 1, 1, -1]
+def bfs(x, y):
+    field[x][y] = 0
+    q = deque()
+    q.append([x, y])
+    while q:
+        a, b = q.popleft()
+        for i in range(8):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and field[nx][ny] == 1:
+                field[nx][ny] = 0
+                q.append([nx, ny])
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+        
+    field = []
+    count = 0
+    for _ in range(h):
+        field.append(list(map(int, input().split())))
+        
+    for i in range(h):
+        for j in range(w):
+            if field[i][j] == 1:
+                bfs(i, j)
+                count += 1
+    print(count)
