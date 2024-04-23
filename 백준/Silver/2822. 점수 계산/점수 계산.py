3dbd94a8b4142907a78b222530dde06af7a0c45d
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-23 22:33:27 +0900

    [Silver V] Title: 점수 계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+score = [int(input()) for _ in range(8)]
+
+temp = []
+answer = 0
+for i in range(5):
+    answer += max(score)
+    temp.append(score.index(max(score)) + 1)
+    score[score.index(max(score))] = -1
+temp.sort()
+
+print(answer)
+print(' '.join(map(str, temp)))
