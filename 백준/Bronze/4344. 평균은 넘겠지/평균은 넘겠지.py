05a2bab1ae503faabf65b05db76b6f3f0edd10d9
Charmull <56704985+Charmull@users.noreply.github.com>
2024-04-26 21:45:57 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:]) / nums[0]
+    cnt = 0
+    for score in nums[1:]:
+        if score > avg:
+            cnt += 1
+    rate = cnt / nums[0] * 100
+    print('{:.3f}%'.format(rate))
