2273a8780e48a9727f2d62072e7490c568a32154
Charmull <56704985+Charmull@users.noreply.github.com>
2024-05-01 23:38:56 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N):
+    a = input()
+    score = 0
+    total = 0
+    for j in a:
+        if j == 'O':
+            score += 1
+        else:
+            score = 0
+        total += score
+    print(total)
