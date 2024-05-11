21bea2af09f5d8f8decb03f971a014470987b0f5
Charmull <56704985+Charmull@users.noreply.github.com>
2024-05-11 21:43:45 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -5,11 +5,20 @@ n, m = map(int, input().split())
 card = list(map(int, input().split()))
 result = 0
 
-for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            tmp = card[i] + card[j] + card[k]
-            if tmp <= m and tmp > result:
-                result = tmp
 
+for i in range(n):
+    if m < card[i]:
+        continue
+
+    for j in range(i + 1, n):
+        if m < card[i] + card[j]:
+            continue
+
+        for k in range(j + 1, n):
+            if result < card[i] + card[j] + card[k] <= m:
+                result = card[i] + card[j] + card[k]
+            if result == m:
+                print(result)
+                sys.exit(0)
+        
 print(result)
